ed85ad2c51b3d630fca5d8ba50dcd7c8f2d53fd8
Valtteri <Valtt3r1@users.noreply.github.com>
2019-04-22 19:19:19 +0300

    Add files via upload

---
+++ Website/JS Crash Course/main.js
@@ -0,0 +1,13 @@
+const name = "John";
+const age = 30;
+
+// Concatenation
+
+console.log("My name is " + name + " and I am " + age)
+
+
+
+
+// Variables tyypit: (var), let (Ei tarvitse arvoa), const (tarvitsee arvon)
+// Data tyypit: string, numbers, boolean, null, undefined
+// Datatyyppien testaus: console.log(typeof rating);
